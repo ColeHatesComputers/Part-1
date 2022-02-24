5c7dc5c53e4c233a2fab1fe8a8004a0758c5138e
Cole Norman <NormCole255@GOTVDSB.CA>
2022-02-24 10:25:59 -0500

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName ="Cole";
+            string favMovie ="The Matrix";
+            favMovie = (favMovie.ToUpper());
+            firstName = (firstName.ToUpper());
+           
+                
+                Console.WriteLine($"Believe it or not {firstName} doesn't like to watch {favMovie}");                               
+        
+            
+
+
+
+
+
+
+
+
+        }
+        
+       
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+}
