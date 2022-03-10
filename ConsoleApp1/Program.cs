ba38fa21959c6f06434b304d1231c113ab238aa3
Cole Norman <NormCole255@GoTVDSB.ca>
2022-03-10 09:53:47 -0500

    it runs now

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,15 +6,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string empty = "";
+            string quote = "The only bar I set low is for limbo";
             string firstName ="Cole";
             string favMovie ="The Matrix";
+            bool b = favMovie.Contains("THE");
+            favMovie = (favMovie.Replace("a", "@").Replace("e", "3"));
             favMovie = (favMovie.ToUpper());
             firstName = (firstName.ToUpper());
-           
+            quote = (quote.Replace("e", "").Replace("a", "").Replace("i", "").Replace("o", "").Replace("u", "").Replace("A", "").Replace("E", "").Replace("I", "").Replace("O", "").Replace("U", ""));
                 
-                Console.WriteLine($"Believe it or not {firstName} doesn't like to watch {favMovie}");                               
-        
-            
+            Console.WriteLine($"Believe it or not {firstName} doesn't like to watch {favMovie}");
+            Console.WriteLine(b);
+            Console.WriteLine(quote);
+            Console.WriteLine("-_-");
+            Console.WriteLine("(.)(.)");
 
 
 
